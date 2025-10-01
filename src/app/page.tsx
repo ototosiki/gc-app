50065aa0923aea31e226323d05b659d8c959cc66
ototosiki <otsuka0125g@gmail.com>
2025-10-01 09:51:37 +0900

    deployerror_edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import Auth from '../components/Auth'
 
 export default function Home() {
-  const key = process.env.SAMPLE_SECRET_KEY;
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-black">
       <h1></h1>
